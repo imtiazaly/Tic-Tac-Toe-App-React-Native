f550e3d3efaeae0b6d5d50727d0ca27ad4a31289
Imtiaz Ali <imtiazali17314@gmail.com>
2026-09-09 10:25:13 +0500

    feat: add Icons component for rendering FontAwesome icons

---
+++ src/components/Icons.tsx
@@ -0,0 +1,24 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { PropsWithChildren } from 'react';
+import FontAwesome from '@react-native-vector-icons/fontawesome';
+
+type IconProps = PropsWithChildren<{
+  name: string;
+}>;
+
+export default function icons({ name }: IconProps) {
+  switch (name) {
+    case 'circle':
+      return <FontAwesome name="circle-thin" size={38} color="#F7CD2E" />;
+      break;
+
+    case 'cross':
+      return <FontAwesome name="times" size={38} color="#38CC77" />;
+      break;
+
+    default:
+      return <FontAwesome name="times" size={38} color="#0D0D0D" />;
+  }
+}
+
+const styles = StyleSheet.create({});
